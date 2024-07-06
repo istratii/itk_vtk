14089b2722ca7741a1bb4121b6b1bfdb41a7401d
Etienne Reverechon <etienne.reverchon@epita.fr>
2024-07-06 11:32:53 +0200

    fix: registred work good & create pipele & README

--- README.md
+++ README.md
@@ -1,8 +1,37 @@
 # SOLUTION
 
-### Recalage d'images
+# Recalage d'Images IRM du Cerveau
 
-On choisit recalage rigide est une méthode utilisée pour aligner deux images en utilisant des transformations rigides, c’est-à-dire des transformations qui incluent uniquement la rotation et la translation. Ce type de recalage est particulièrement utile lorsque les images proviennent du même patient et que nous supposons qu’il n’y a pas de déformation ou de changement de forme entre les deux scans, seulement des déplacements et des rotations.
+Ce projet se concentre sur le recalage d'images IRM 3D du cerveau afin de faciliter l'analyse et la détection de tumeurs. Plusieurs méthodes de recalage ont été testées pour identifier la plus efficace. Voici un résumé des méthodes testées et des raisons pour lesquelles nous avons retenu la méthode finale.
+
+## Méthodes Testées
+
+1. **Transformation de Translation**
+   - **Description** : Aligne les images en ajustant uniquement la translation.
+   - **Résultats** : Offre un alignement rapide et efficace, avec des résultats satisfaisants pour les images IRM du cerveau dans ce projet spécifique. Cette méthode a produit les meilleurs résultats en termes de rapidité et de qualité d'alignement.
+
+2. **Transformation Affine**
+   - **Description** : Prend en compte la translation, la rotation, l'échelle et le cisaillement.
+   - **Résultats** : Meilleure que la transformation de translation, mais introduit parfois des distorsions et n'est pas toujours stable pour les structures anatomiques complexes.
+
+3. **Transformation Rigide (Euler 3D Transform)**
+   - **Description** : Prend en compte la translation et la rotation pour une transformation de corps rigide. Implémentée avec une stratégie multi-résolution pour améliorer la convergence et la précision.
+   - **Résultats** : Offre un alignement précis mais est plus lente et nécessite des ajustements fins des paramètres pour une bonne convergence. Dans ce projet, elle n'a pas surpassé la méthode de translation en termes de résultats pratiques.
+
+## Méthode Retenue
+
+**Transformation de Translation**
+- **Précision** : Offre un alignement suffisant pour les images IRM du cerveau sans nécessiter de rotations ou de changements d'échelle.
+- **Rapidité** : Plus rapide à exécuter, ce qui est crucial pour des traitements en temps réel ou des analyses nécessitant un traitement rapide.
+- **Simplicité** : Moins de paramètres à ajuster, réduisant ainsi la complexité et le risque de divergence.
+
+## Paramètres Optimisés
+
+Pour optimiser le recalage, les paramètres suivants ont été utilisés :
+- **Taux d'apprentissage** : 4.0
+- **Nombre d'itérations** : 200
+- **Stratégie multi-résolution** : Un niveau de résolution
+
 
 ### Segmentation des tumeurs
 
--- src/main.py
+++ src/main.py
@@ -1,2 +1,93 @@
-if __name__ == "__main__":
-    print("Hello world!")
+
+import itk
+import matplotlib.pyplot as plt
+
+def registred_images(fixed_filepath, moving_filepath, output_filepath):
+    PixelType = itk.F
+    fixed_image = itk.imread(fixed_filepath, PixelType)
+    moving_image = itk.imread(moving_filepath, PixelType)
+
+    dimension = fixed_image.GetImageDimension()
+    FixedImageType = type(fixed_image)
+    MovingImageType = type(moving_image)
+
+    TransformType = itk.TranslationTransform[itk.D, dimension]
+    initialTransform = TransformType.New()
+
+    optimizer = itk.RegularStepGradientDescentOptimizerv4.New()
+
+    optimizer.SetLearningRate(4)
+    optimizer.SetMinimumStepLength(0.001)
+    optimizer.SetNumberOfIterations(200)
+
+    metric = itk.MeanSquaresImageToImageMetricv4[FixedImageType, MovingImageType].New()
+    fixed_interpolation = itk.LinearInterpolateImageFunction[FixedImageType, itk.D].New()
+    metric.SetFixedInterpolator(fixed_interpolation)
+
+    registration = itk.ImageRegistrationMethodv4[FixedImageType, FixedImageType].New(               FixedImage=fixed_image, 
+                                               MovingImage=moving_image, 
+                                               Metric=metric,
+                                               Optimizer=optimizer, 
+                                               InitialTransform=initialTransform)
+
+    moving_initial_transform = TransformType.New()
+    initial_parameters = moving_initial_transform.GetParameters()
+    initial_parameters.Fill(0)
+    moving_initial_transform.SetParameters(initial_parameters)
+    registration.SetMovingInitialTransform(moving_initial_transform)
+
+    identity_transform = TransformType.New()
+    identity_transform.SetIdentity()
+    registration.SetFixedInitialTransform(identity_transform)
+
+    registration.SetNumberOfLevels(1)
+
+    registration.Update()
+
+    transform = registration.GetTransform()
+    final_parameters = transform.GetParameters()
+    translation = [final_parameters.GetElement(i) for i in range(dimension)]
+
+    number_of_iterations = optimizer.GetCurrentIteration()
+
+    best_value = optimizer.GetValue()
+
+    print("Result = ")
+    for i in range(dimension):
+        print(f" Translation along axis {i} = {translation[i]}")
+    print(" Iterations    = " + str(number_of_iterations))
+    print(" Metric value  = " + str(best_value))
+
+    CompositeTransformType = itk.CompositeTransform[itk.D, dimension]
+    output_composite_transform = CompositeTransformType.New()
+    output_composite_transform.AddTransform(moving_initial_transform)
+    output_composite_transform.AddTransform(registration.GetModifiableTransform())
+
+    resampler = itk.ResampleImageFilter.New(Input=moving_image, Transform=transform, UseReferenceImage=True,
+                                            ReferenceImage=fixed_image)
+    resampler.SetDefaultPixelValue(100)
+
+    OutputPixelType = itk.ctype('unsigned char')
+    OutputImageType = itk.Image[OutputPixelType, dimension]
+    caster = itk.CastImageFilter[FixedImageType, OutputImageType].New(resampler)
+
+    itk.imwrite(caster, output_filepath)
+
+    print(f"Registered image saved to {output_filepath}")
+
+
+
+
+def main():
+    # Dmages
+    fixed_filepath = '../Data/case6_gre1.nrrd'
+    moving_filepath = '../Data/case6_gre2.nrrd'
+    output_filepath = '../Data/brain-registered.nrrd'
+    # Recalage 
+    registred_images(fixed_filepath, moving_filepath, output_filepath)
+    # Segmentation
+    # TO DO 
+    # Visulasition 
+    # TO DO 
+    
+main()
